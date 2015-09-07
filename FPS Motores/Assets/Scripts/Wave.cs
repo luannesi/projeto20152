652ed0d7cbeb35d496ef73d2f46a834bd5abe93f
EdGRESSLER <dudugress@hotmail.com>
2015-09-06 21:12:39 -0300

    Correção level Eduardo
    ocorreu alguns problemas na minha pasta, acabei baixando a pasta pelo
    site do github, sim é a vesão mais recente, e coloquei sobre o meu
    projeto, por as 1871 modificações e também coloquei meu level.

---
+++ FPS Motores/Assets/Scripts/Wave.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Wave
+{
+    public string name;
+    public List<WaveEnemies> Monsters;
+}
+ 
+
---
+++ FPS Motores/Assets/Scripts/WaveEnemies.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class WaveEnemies
+{
+    [Header("Tempo de Spawn para cada monstro")]
+    public float delay;
+    public Transform prefab;
+    [Header("Quantidade de monstros deste tipo")]
+    public int Count;
+}
